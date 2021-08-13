1e07040e1687e662f37083ec08fee9e9dff4ae73
Sahil Shubham <soilshubham@gmail.com>
2021-08-13 12:20:17 +0530

    improved the design of loading dummy subject cards

--- src/components/SubjectCard/SubCard.js
+++ src/components/SubjectCard/SubCard.js
@@ -9,29 +9,47 @@ import { Card, Button } from "react-bootstrap";
 
 class SubCard extends React.Component {
   render() {
-    return (
-      <Card className={"subcard " + this.props.color}>
-        <div className="cover-container">
-          <div className="overlay"></div>
-          <Card.Img className="cover" variant="top" src={this.props.cover} />
-        </div>
-        <Card.Body>
-          <Card.Title className="title">
-            <FontAwesomeIcon className="fa-icon" icon={faBook} />
-            {this.props.name}
-          </Card.Title>
-          <hr />
-          {/* <Card.Text className="text">{this.props.desc}</Card.Text> */}
-          <Link to={"/sub/" + this.props.sub}>
-            <Button className="btn">
-              {this.props.sub.length > 0 ? "Get Resources" : "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"}
-
-              <FontAwesomeIcon className="fa-icon" icon={faArrowRight} />
+    if (this.props.sub.length > 0) {
+      return (
+        <Card className={"subcard " + this.props.color}>
+          <div className="cover-container">
+            <div className="overlay"></div>
+            <Card.Img className="cover" variant="top" src={this.props.cover} />
+          </div>
+          <Card.Body>
+            <Card.Title className="title">
+              <FontAwesomeIcon className="fa-icon" icon={faBook} />
+              {this.props.name}
+            </Card.Title>
+            <hr />
+            {/* <Card.Text className="text">{this.props.desc}</Card.Text> */}
+            <Link to={"/sub/" + this.props.sub}>
+              <Button className="btn">
+                Get Resources
+                <FontAwesomeIcon className="fa-icon" icon={faArrowRight} />
+              </Button>
+            </Link>
+          </Card.Body>
+        </Card>
+      );
+    } else {
+      return (
+        <Card className={"subcard-loading " + this.props.color}>
+          <div className="cover-container">
+            <div className="overlay"></div>
+            <Card.Img className="cover" variant="top" src={""} />
+          </div>
+          <Card.Body>
+            <Card.Title className="title">⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀</Card.Title>
+            <hr />
+            {/* <Card.Text className="text">{this.props.desc}</Card.Text> */}
+            <Button className="btn" disabled>
+              ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
             </Button>
-          </Link>
-        </Card.Body>
-      </Card>
-    );
+          </Card.Body>
+        </Card>
+      );
+    }
   }
 }
 
